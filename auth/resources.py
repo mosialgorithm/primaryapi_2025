3fb181a882e8aa266c39df62493ec58e10b76ed5
mosialgorithm <mosiighorbani@gmail.com>
2025-06-15 02:28:40 +0330

    register , login, whoami auth is worked successfully

--- auth/resources.py
+++ auth/resources.py
@@ -5,7 +5,7 @@ from flask_smorest import Blueprint, abort
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, get_jwt, jwt_required
 from app import db, limiter
 from user.models import UserModel
-from user.schemas import UserSchema
+from user.schemas import UserSchema, UserLoginSchema, UserRegisterSchema
 from blocklist import BLOCKLIST
 
 
@@ -14,7 +14,7 @@ auth = Blueprint("Auth", __name__, url_prefix='/api/auth', description = "Authen
 
 @auth.route("/register")
 class UserRegisterView(MethodView):
-    @auth.arguments(UserSchema)
+    @auth.arguments(UserRegisterSchema)
     @auth.response(201, UserSchema)
     @limiter.limit("100/hour")
     def post(self, user_data):
@@ -39,3 +39,46 @@ class UserRegisterView(MethodView):
         except Exception as ex:
             db.session.rollback()
             return jsonify({"message": f"Error {ex} is Happened"}), 400
+        
+@auth.route('/login')
+class UserloginView(MethodView):
+    @auth.arguments(UserLoginSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.phone == user_data['phone']).first()
+        if user:
+            if user.check_password(user_data['password']):
+                access_token = create_access_token(identity=str(user.id), fresh=True)
+                refresh_token = create_refresh_token(str(user.id))
+                return jsonify({
+                    "access token" : access_token,
+                    "refresh token" : refresh_token,
+                }), 200
+            else:
+                return jsonify({
+                    "message" : "password is wrong",
+                    "status" : "wrong"
+                })
+        else:
+            return jsonify({
+                    "message" : "user is not found !!",
+                    "status" : "wrong"
+                })
+        
+@auth.route('/logout')
+class UserLogoutView(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()['jti']
+        BLOCKLIST.add(jti)
+        return jsonify({
+            "message" : "User is Remove from Server",
+            "status":"remove"
+        }), 200
+    
+@auth.route('/whoami')
+class UserStatusView(MethodView):
+    @auth.response(200, UserSchema)
+    @jwt_required()
+    def get(self):
+        user = UserModel.query.filter_by(id=get_jwt_identity()).one_or_none()
+        return user
--- user/schemas.py
+++ user/schemas.py
@@ -4,11 +4,26 @@ from flask_smorest.fields import Upload
 
 
 class UserSchema(Schema):
-    id = fields.Int()
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=False)
+    username = fields.Str(required=False)
+    phone = fields.Str(required=True)
+    email = fields.Str(required=False)
+    password = fields.Str(required=True, load_only=True)
+    role = fields.Integer(dump_only=True)
+    avatar = fields.Str(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
+
+
+class UserRegisterSchema(Schema):
     name = fields.Str(required=False)
     username = fields.Str(required=False)
     phone = fields.Str(required=True)
     email = fields.Str(required=False)
     password = fields.Str(required=True, load_only=True)
 
+
+class UserLoginSchema(Schema):
+    phone = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
     
